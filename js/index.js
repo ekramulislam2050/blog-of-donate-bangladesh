a34c2180d1907c1cdab82e3e376bdb924fe8b58c
ekramulislam2050 <ekramulislam2050@gmail.com>
2024-09-23 19:07:51 +0600

    add and update functionality added for card-1 and nav-bar available money button

--- js/index.js
+++ js/index.js
@@ -5,5 +5,15 @@ document.getElementById("blog-btn").addEventListener("click",function(e){
  
 document.getElementById("card-1-donate-btn").addEventListener("click",function(){
     const donatedBdt = getDonateAmount("card-1-input-field");
-     const availableBdt = 
+    if(isNaN(donatedBdt) || 0>donatedBdt){
+        alert("Invalid input")
+        return
+    }
+    const availableCardOneBdt = getAvailableAmount("card-1-bdt");
+    const currentCardOneBdt = availableCardOneBdt + donatedBdt;
+    document.getElementById("card-1-bdt").innerText = currentCardOneBdt ;
+    const availableNavBdt = getAvailableAmount("nav-bdt" );
+    const currentNavBdt = availableNavBdt + donatedBdt;
+    document.getElementById("nav-bdt").innerText = currentNavBdt
+
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,5 +1,10 @@
 function getDonateAmount(id){
   const donateAmount = document.getElementById(id).value ;
-  const convertToNumber=parseFloat(donateAmount);
+  const convertToNumber = parseFloat(donateAmount);
   return convertToNumber;
+}
+function getAvailableAmount(id){
+    const availableAmount = document.getElementById(id).innerText;
+    const convertToNumber = parseFloat(availableAmount);
+    return convertToNumber
 }
