1b7fa443966f1e9e31cc466a3d124aca756b0d0b
ekramulislam2050 <ekramulislam2050@gmail.com>
2024-09-24 13:07:00 +0600

    working

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ document.getElementById("card-1-donate-btn").addEventListener("click",function()
         alert("Invalid input")
         return
     }
+    createElement(donatedBdt,"card-1-h2")
     const availableCardOneBdt = getAvailableAmount("card-1-bdt");
     const currentCardOneBdt = availableCardOneBdt + donatedBdt;
     document.getElementById("card-1-bdt").innerText = currentCardOneBdt +' BDT' ;
@@ -16,7 +17,7 @@ document.getElementById("card-1-donate-btn").addEventListener("click",function()
     const currentNavBdt = availableNavBdt + donatedBdt;
     document.getElementById("nav-bdt").innerText = currentNavBdt +' BDT';
     giveCongratulation("my_modal_1")
-    
+   
 })
 // card-2-------------------------------->
 document.getElementById("card-2-donate-btn").addEventListener("click",function(){
@@ -25,6 +26,7 @@ document.getElementById("card-2-donate-btn").addEventListener("click",function()
         alert("Invalid input")
         return
     }
+    createElement(donatedBdt)
     const availableCardTwoBdt = getAvailableAmount("card-2-bdt");
     const currentCardTwoBdt = availableCardTwoBdt + donatedBdt;
     document.getElementById("card-2-bdt").innerText = currentCardTwoBdt +' BDT' ;
@@ -40,6 +42,7 @@ document.getElementById("card-3-donate-btn").addEventListener("click",function()
         alert("Invalid input")
         return
     }
+    createElement(donatedBdt)
     const availableCardThreeBdt = getAvailableAmount("card-3-bdt");
     const currentCardThreeBdt = availableCardThreeBdt + donatedBdt;
     document.getElementById("card-3-bdt").innerText = currentCardThreeBdt +' BDT';
@@ -47,4 +50,19 @@ document.getElementById("card-3-donate-btn").addEventListener("click",function()
     const currentNavBdt = availableNavBdt + donatedBdt;
     document.getElementById("nav-bdt").innerText = currentNavBdt +' BDT'
     giveCongratulation("my_modal_1")
+})
+// history button----------------------------->
+document.getElementById("history-btn").addEventListener("click",function(e){
+ 
+    hide("card-1","card-2","card-3","history-section" );
+
+    e.target.style.background='rgb(187, 245, 112)';
+    document.getElementById("donation-btn").style.background='none';
+    createElement()
+});
+// donation button--------------------------->
+document.getElementById("donation-btn").addEventListener("click",function(e){
+    show("card-1","card-2","card-3","history-section");
+    e.target.style.background='rgb(187, 245, 112)';
+    document.getElementById("history-btn").style.background='none';
 })
--- js/utilities.js
+++ js/utilities.js
@@ -14,3 +14,35 @@ function giveCongratulation (id){
   const showModal = myModal.showModal();
   return showModal;
 }
+
+function hide(id_1,id_2,id_3,h_s){
+  document.getElementById(id_1).classList.add("hidden");
+  document.getElementById(id_2).classList.add("hidden");
+  document.getElementById(id_3).classList.add("hidden");
+  document.getElementById(h_s).classList.remove("hidden");
+}
+function show(id_1,id_2,id_3,h_s){
+  document.getElementById(id_1).classList.remove("hidden");
+  document.getElementById(id_2).classList.remove("hidden");
+  document.getElementById(id_3).classList.remove("hidden");
+  document.getElementById(h_s).classList.add("hidden");
+}
+
+function createElement(donatedTk,id){
+     const heading = document.getElementById(id);
+     console.log(heading)
+     const historySection = document.getElementById("history-section");
+     const newDiv = document.createElement("div");
+     newDiv.style.border='1px solid gray';
+     newDiv.style.borderRadius='5px';
+     newDiv.style.width='80%';
+     newDiv.style.margin='2px auto';
+     newDiv.innerHTML=`
+      <h1 class="text-xl text-center">${donatedTk} tk </h1>
+      <p class="text-center">${new Date()}</p>
+     `
+     historySection.appendChild(newDiv)
+    
+    
+   
+}
